b46fdc06792f2a0eb6af7681f3e1772c96b473b5
Harriet HW <harriethw@gmail.com>
2020-11-21 18:00:30 +0000

    Add express server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT);
